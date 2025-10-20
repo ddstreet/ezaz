7bb29d8db115f33b7d5b8b4cf26a1f19f6d9587a
Dan Streetman <ddstreet@ieee.org>
2025-10-20 12:29:16 -0400

    add more filters to vm instance type

--- ezaz/azobject/vminstancetype.py
+++ ezaz/azobject/vminstancetype.py
@@ -21,6 +21,10 @@ class VmInstanceType(ComputeSku):
                                            instance_mem_gb=dict(infoattr=cls.get_capability_infogetter('MemoryGB'),
                                                                 help='Use instance type with this many GB of memory'),
                                            instance_architecture=dict(infoattr=cls.get_capability_infogetter('CpuArchitectureType'),
-                                                                      help='Use instance type with this CPU architecture')),
+                                                                      help='Use instance type with this CPU architecture'),
+                                           instance_hyperv_gen=dict(infoattr=cls.get_capability_infogetter('HyperVGenerations'),
+                                                                    help='Use instance type that supports this Hyper-V generation'),
+                                           instance_disk_controller=dict(infoattr=cls.get_capability_infogetter('DiskControllerTypes'),
+                                                                         help='Use instance type that supports disk controller type (e.g. NVMe)')),
                                       azclass=cls,
                                       **kwargs)
